0da9916fe7894d21c309c799ece827532cf044de
pnguyen59411822 <pnguyen59411822@gmail.com>
2023-05-04 01:15:22 +0700

    [main] decrease font size of text boxes

--- Bai1_KetNoi/Frm_main.cs
+++ Bai1_KetNoi/Frm_main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,25 @@ namespace Bai1_KetNoi
 {
     public partial class frm_main : Form
     {
+        private string get_connectionString()
+        {
+            string str = "Server=" + txt_server.Text
+                       + ";database=" + txt_database.Text;
+
+            if (rbtn_win.Checked == true)
+            {
+                str += ";integrated security=true";
+            }
+
+            else
+            {
+                str += ";uid=" + txt_user.Text
+                    + ";pwd=" + txt_pass.Text;
+            }
+
+            return str;
+        }
+
         public frm_main()
         {
             InitializeComponent();
@@ -19,8 +39,14 @@ namespace Bai1_KetNoi
 
         private void frm_main_Load(object sender, EventArgs e)
         {
+            const string DEFAULT_SERVER = "DESKTOP-L72T38U\\SQLEXPRESS";
+            const string DEFAULT_DATABASE = "";
+
             lb_user.ForeColor = Color.LightSteelBlue;
             lb_pass.ForeColor = Color.LightSteelBlue;
+
+            txt_server.Text = DEFAULT_SERVER;
+            txt_database.Text = DEFAULT_DATABASE;
 
             txt_user.Enabled = false;
             txt_pass.Enabled = false;
@@ -52,7 +78,20 @@ namespace Bai1_KetNoi
 
         private void btn_connect_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                SqlConnection cnn = new SqlConnection();
+                cnn.ConnectionString = get_connectionString();
+                cnn.Open();
+
+                MessageBox.Show("Kết nối sql thành công");
+                cnn.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kết nối sql thất bại. Nguyên nhân:\n" + ex.Message);
+            }
         }
     }
 }
