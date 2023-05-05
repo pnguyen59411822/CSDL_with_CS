4729543a973660c61f425e0a313c39e5d6b801b7
pnguyen59411822 <pnguyen59411822@gmail.com>
2023-05-06 00:08:18 +0700

    [Frm_main] Design inputs

--- Bai2_Command/Form1.Designer.cs
+++ Bai2_Command/Form1.Designer.cs
@@ -28,7 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_maKH = new System.Windows.Forms.TextBox();
+            this.txt_tenKH = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_diaChi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_SDT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(34, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã khách hàng";
+            // 
+            // txt_maKH
+            // 
+            this.txt_maKH.Enabled = false;
+            this.txt_maKH.Location = new System.Drawing.Point(197, 17);
+            this.txt_maKH.Name = "txt_maKH";
+            this.txt_maKH.Size = new System.Drawing.Size(689, 23);
+            this.txt_maKH.TabIndex = 1;
+            // 
+            // txt_tenKH
+            // 
+            this.txt_tenKH.Enabled = false;
+            this.txt_tenKH.Location = new System.Drawing.Point(197, 56);
+            this.txt_tenKH.Name = "txt_tenKH";
+            this.txt_tenKH.Size = new System.Drawing.Size(689, 23);
+            this.txt_tenKH.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(34, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên khách hàng";
+            // 
+            // txt_diaChi
+            // 
+            this.txt_diaChi.Enabled = false;
+            this.txt_diaChi.Location = new System.Drawing.Point(197, 96);
+            this.txt_diaChi.Name = "txt_diaChi";
+            this.txt_diaChi.Size = new System.Drawing.Size(689, 23);
+            this.txt_diaChi.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Location = new System.Drawing.Point(34, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Địa chỉ";
+            // 
+            // txt_SDT
+            // 
+            this.txt_SDT.Enabled = false;
+            this.txt_SDT.Location = new System.Drawing.Point(197, 136);
+            this.txt_SDT.Name = "txt_SDT";
+            this.txt_SDT.Size = new System.Drawing.Size(689, 23);
+            this.txt_SDT.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label4.Location = new System.Drawing.Point(34, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Số điện thoại";
             // 
             // Frm_main
             // 
@@ -36,15 +120,33 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(934, 519);
+            this.Controls.Add(this.txt_SDT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_diaChi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_tenKH);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_maKH);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Frm_main";
             this.Text = "Danh sách khách hàng";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_maKH;
+        private System.Windows.Forms.TextBox txt_tenKH;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_diaChi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_SDT;
+        private System.Windows.Forms.Label label4;
     }
 }
 
