279fcaee0e7642ea482dfc5e133baa16401c1c57
pnguyen59411822 <pnguyen59411822@gmail.com>
2023-05-07 13:03:52 +0700

    [Frm_main] Edit this form properties - Font - Text

--- Bai3_Adapter/Frm_main.cs
+++ Bai3_Adapter/Frm_main.cs
@@ -1,8 +1,8 @@
 namespace Bai3_Adapter
 {
-    public partial class Form1 : Form
+    public partial class Frm_main : Form
     {
-        public Form1()
+        public Frm_main()
         {
             InitializeComponent();
         }
