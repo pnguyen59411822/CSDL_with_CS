85eda242052139747b99f7d3f1ae9fbce50dd761
pnguyen59411822 <pnguyen59411822@gmail.com>
2023-05-09 03:04:09 +0700

    [Frm_detail] Show data of selected row in Frm_main

--- Bai3_Adapter/Frm_detail.cs
+++ Bai3_Adapter/Frm_detail.cs
@@ -17,9 +17,21 @@ namespace Bai3_Adapter
             InitializeComponent();
         }
 
-        public Frm_detail(BindingSource bds)
+        public Frm_detail(BindingSource source)
         {
             InitializeComponent();
+
+            BindingSource bds = source;
+
+            txt_maKH.DataBindings.Clear();
+            txt_tenKH.DataBindings.Clear();
+            txt_diaChi.DataBindings.Clear();
+            txt_SDT.DataBindings.Clear();
+
+            txt_maKH.DataBindings.Add("Text", bds, "MaKH", true, DataSourceUpdateMode.OnPropertyChanged);
+            txt_tenKH.DataBindings.Add("Text", bds, "TenKH", true, DataSourceUpdateMode.OnPropertyChanged);
+            txt_diaChi.DataBindings.Add("Text", bds, "DCKH", true, DataSourceUpdateMode.OnPropertyChanged);
+            txt_SDT.DataBindings.Add("Text", bds, "DTKH", true, DataSourceUpdateMode.OnPropertyChanged);
         }    
 
         private void Frm_detail_Load(object sender, EventArgs e)
