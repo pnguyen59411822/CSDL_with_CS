d117663ca508d0b1631c771fde3a0af7e689334c
pnguyen59411822 <pnguyen59411822@gmail.com>
2023-05-08 20:17:20 +0700

    [Frm_main] Change properties - Font - Text

--- Bai3_Adapter/Frm_main.cs
+++ Bai3_Adapter/Frm_main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Bai3_Adapter
 {
-    public partial class Form1 : Form
+    public partial class Frm_main : Form
     {
-        public Form1()
+        public Frm_main()
         {
             InitializeComponent();
         }
